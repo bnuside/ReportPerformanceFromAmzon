e8ce0ea24283734afd199199aec54ec6330ac7f7
bnuside <bnuside@gmail.com>
2019-06-03 17:51:13 +0800

    [refactor] get matrix data.

--- libs/db_helper.py
+++ libs/db_helper.py
@@ -1,6 +1,8 @@
 import pymysql
-import time
+import os
 from libs.log import Log
+from libs.env import Env
+from libs.common import get_cdate
 
 class Database(object):
     def __init__(self):
@@ -13,6 +15,7 @@ class Database(object):
         self._log = Log()
         self._db_koala = None
         self._db_matrix = None
+        self._env = Env()
 
     def _connect_database_server(self, db_name):
         try:
@@ -52,28 +55,33 @@ class Database(object):
         except Exception as e:
             self._log.logger.error(e)
 
-    def _get_cdate(self, x_day_before, today=None):
-        if not today:
-            today = time.time()
-        day_seconds = 60 * 60 * 24
-        target_timestamp = today - day_seconds * x_day_before
-        target_date = time.localtime(target_timestamp)
-        year = target_date.tm_year
-        if target_date.tm_mon < 10:
-            month = '0%s' % target_date.tm_mon
-        else:
-            month = target_date.tm_mon
-
-        if target_date.tm_mday < 10:
-            day = '0%s' % target_date.tm_mday
-        else:
-            day = target_date.tm_mday
-
-        return '%s%s%s' % (year, month, day), '%s-%s-%s' % (year, month, day)
-
     def get_latest_version_data(self, app):
         db_koala = self._connect_koala_database()
         cursor_koala = self._get_cursor(db_koala)
-        _, formated_cdate = self._get_cdate(2)
-        data = self._run_sql_and_commit(db_koala, cursor_koala, app.get_version_sql(formated_cdate))
+        _, formated_cdate = get_cdate(2)
+        data = self._run_sql_and_commit(db_koala, cursor_koala, self._get_version_sql(app, formated_cdate))
         return data
+
+    def get_matrix_data(self, app, formated_cdate, formated_versions):
+        sql = self._get_performance_data_sql(app, formated_cdate, formated_versions)
+        return self.execute_athena_sql(sql)
+
+    def _get_version_sql(self, app, formated_cdate):
+        """
+
+        :param formated_cdate: 类似「2019-05-28」这样格式的日期
+        :return:
+        """
+
+        return "select g_key from app_details_dlu where create_date='{cdate}' and app_key='{appkey}' and gby_type='appversion' and g_value>10000 order by CAST(SUBSTRING_INDEX(g_key, '.', -1) AS UNSIGNED) desc limit 5;".format(
+            cdate=formated_cdate, appkey=app.app_key)
+
+    def _get_performance_data_sql(self, app, cdate, formated_versions):
+        return "select cdate,app_key,version,nation, avg(case when json_extract_scalar(extra,'$.app_create') is not null and  json_extract_scalar(extra,'$.app_create')<>'' and cast( json_extract_scalar(extra,'$.app_create') as BIGINT) >0 and cast( json_extract_scalar(extra,'$.app_create') as BIGINT) <20000 then cast( json_extract_scalar(extra,'$.app_create') as BIGINT) end) as app_create_time, avg(case when json_extract_scalar(extra,'$.kb_create_1st') is not null and  json_extract_scalar(extra,'$.kb_create_1st')<>'' and cast( json_extract_scalar(extra,'$.kb_create_1st') as BIGINT)>0 and cast( json_extract_scalar(extra,'$.kb_create_1st') as BIGINT)<20000 then cast( json_extract_scalar(extra,'$.kb_create_1st') as BIGINT) end) as kb_create_1st_time, avg(case when json_extract_scalar(extra,'$.kb_createview_startup') is not null and  json_extract_scalar(extra,'$.kb_createview_startup')<>'' and cast( json_extract_scalar(extra,'$.kb_createview_startup') as BIGINT)>0 and cast( json_extract_scalar(extra,'$.kb_createview_startup') as BIGINT)<20000 then cast( json_extract_scalar(extra,'$.kb_createview_startup') as BIGINT) end) as kb_createview_startup_time, avg(case when json_extract_scalar(extra,'$.kb_createview_startup_1st') is not null and  json_extract_scalar(extra,'$.kb_createview_startup_1st')<>'' and cast( json_extract_scalar(extra,'$.kb_createview_startup_1st') as BIGINT)>0 and cast( json_extract_scalar(extra,'$.kb_createview_startup_1st') as BIGINT)<20000 then cast( json_extract_scalar(extra,'$.kb_createview_startup_1st') as BIGINT) end) as kb_createview_startup_1st_time, avg(case when json_extract_scalar(extra,'$.kb_warm_startup') is not null and  json_extract_scalar(extra,'$.kb_warm_startup')<>'' and cast( json_extract_scalar(extra,'$.kb_warm_startup') as BIGINT)>0 and cast( json_extract_scalar(extra,'$.kb_warm_startup') as BIGINT)<20000 then cast( json_extract_scalar(extra,'$.kb_warm_startup') as BIGINT) end) as kb_warm_startup_time, avg(case when json_extract_scalar(extra,'$.emoji_time') is not null and  json_extract_scalar(extra,'$.emoji_time')<>'' and cast( json_extract_scalar(extra,'$.emoji_time') as BIGINT)>0 and cast( json_extract_scalar(extra,'$.emoji_time') as BIGINT)<20000 then cast( json_extract_scalar(extra,'$.emoji_time') as BIGINT) end) as emoji_time, avg(case when json_extract_scalar(extra,'$.menu_time') is not null and  json_extract_scalar(extra,'$.menu_time')<>'' and cast( json_extract_scalar(extra,'$.menu_time') as BIGINT)>0 and cast( json_extract_scalar(extra,'$.menu_time') as BIGINT)<20000 then cast( json_extract_scalar(extra,'$.menu_time') as BIGINT) end) as menu_time, avg(case when json_extract_scalar(extra,'$.suggestions_time') is not null and  json_extract_scalar(extra,'$.suggestions_time')<>'' and cast( json_extract_scalar(extra,'$.suggestions_time') as BIGINT)>0 and cast( json_extract_scalar(extra,'$.suggestions_time') as BIGINT)<20000 then cast( json_extract_scalar(extra,'$.suggestions_time') as BIGINT) end) as suggestions_time, avg(case when json_extract_scalar(extra,'$.slide_suggestions_time') is not null and  json_extract_scalar(extra,'$.slide_suggestions_time')<>'' and cast( json_extract_scalar(extra,'$.slide_suggestions_time') as BIGINT) >0 and cast( json_extract_scalar(extra,'$.slide_suggestions_time') as BIGINT) <20000 then cast( json_extract_scalar(extra,'$.slide_suggestions_time') as BIGINT) end) as slide_suggestions_time, approx_percentile(case when json_extract_scalar(extra,'$.app_create') is not null and  json_extract_scalar(extra,'$.app_create')<>'' then cast( json_extract_scalar(extra,'$.app_create') as BIGINT) end,0.95) as app_create_time_95, approx_percentile(case when json_extract_scalar(extra,'$.kb_create_1st') is not null and  json_extract_scalar(extra,'$.kb_create_1st')<>'' then cast( json_extract_scalar(extra,'$.kb_create_1st') as BIGINT) end,0.95) as kb_create_1st_time_95, approx_percentile(case when json_extract_scalar(extra,'$.kb_createview_startup') is not null and  json_extract_scalar(extra,'$.kb_createview_startup')<>'' then cast( json_extract_scalar(extra,'$.kb_createview_startup') as BIGINT) end,0.95) as kb_createview_startup_time_95, approx_percentile(case when json_extract_scalar(extra,'$.kb_createview_startup_1st') is not null and  json_extract_scalar(extra,'$.kb_createview_startup_1st')<>'' then cast( json_extract_scalar(extra,'$.kb_createview_startup_1st') as BIGINT) end,0.95) as kb_createview_startup_1st_time_95, approx_percentile(case when json_extract_scalar(extra,'$.kb_warm_startup') is not null and  json_extract_scalar(extra,'$.kb_warm_startup')<>'' then cast( json_extract_scalar(extra,'$.kb_warm_startup') as BIGINT) end,0.95) as kb_warm_startup_time_95, approx_percentile(case when json_extract_scalar(extra,'$.emoji_time') is not null and  json_extract_scalar(extra,'$.emoji_time')<>'' then cast( json_extract_scalar(extra,'$.emoji_time') as BIGINT) end,0.95) as emoji_time_95, approx_percentile(case when json_extract_scalar(extra,'$.menu_time') is not null and  json_extract_scalar(extra,'$.menu_time')<>'' then cast( json_extract_scalar(extra,'$.menu_time') as BIGINT) end,0.95) as menu_time_95, approx_percentile(case when json_extract_scalar(extra,'$.suggestions_time') is not null and  json_extract_scalar(extra,'$.suggestions_time')<>'' then cast( json_extract_scalar(extra,'$.suggestions_time') as BIGINT) end,0.95) as suggestions_time_95, approx_percentile(case when json_extract_scalar(extra,'$.slide_suggestions_time') is not null and  json_extract_scalar(extra,'$.slide_suggestions_time')<>'' then cast( json_extract_scalar(extra,'$.slide_suggestions_time') as BIGINT) end,0.95) as slide_suggestions_time_95  from gimbal.event where app_key='{appkey}' and layout='performance' and item='key_metrics' and cdate={cdate} and event_part='other'  and version in {ver} and nation in ('us','br','id','mx','ph','ru','ar','co','fr','es') group by cdate,app_key,version,nation".format(appkey=app.appkey, cdate=cdate, ver=formated_versions)
+
+    def execute_athena_sql(self, sql):
+        cmd = '{athena_executor} "{s}"'.format(athena_executor=self._env.athena_executor, s=sql)
+        raw = os.popen(cmd)
+        ret = raw.read()
+        raw.close()
+        return ret
--- run_performance.py
+++ run_performance.py
@@ -1,11 +1,30 @@
 from libs.db_helper import Database
 from libs.apps import Kika2019
+from libs.log import Log
+from libs.common import get_cdate
 
 def make_data():
+    log = Log()
     kika2019 = Kika2019()
     database = Database()
     version_data = database.get_latest_version_data(kika2019)
-    print(version_data)
+    cdate, _ = get_cdate(2)
+    try:
+        version_data = [v[0] for v in version_data]
+        version_data = ','.join(version_data)
+        version_data = '(%s)' % version_data
+        print(version_data)
+
+        matrix_data = database.get_matrix_data(kika2019, cdate, version_data)
+        print(matrix_data)
+    except Exception as e:
+        log.logger.info(e)
+
+
+def debug():
+    v = (('6.6.9.4456',), ('6.6.9.4446',), ('6.6.9.4432',), ('6.6.9.4420',), ('6.6.9.4406',))
+    v = [vv[0] for vv in v]
+    print(','.join(v))
 
 
 if __name__ == '__main__':
